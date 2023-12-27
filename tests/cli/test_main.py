d0cdc186e881c0ead3dfcebd1becf4bdc46d9944
Alix Lahuec <alix.lahuec@dialogue.co>
2023-12-27 17:19:11 -0500

    tests: unstyle stdout in failing snapshot test

--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,3 +1,4 @@
+from click import unstyle
 from typer.testing import CliRunner
 
 from fastapi_checks.cli.main import app
@@ -10,4 +11,4 @@ def test_command_help(base_args, snapshot):
     res = runner.invoke(app, [*base_args, "--help"])
 
     assert res.exit_code == 0
-    assert res.stdout == snapshot
+    assert unstyle(res.stdout) == snapshot
